ceec68a89776921854400d32fb571991cd734e83
RdzSebastian <RdzSebastian@gmail.com>
2019-06-20 16:58:30 -0300

    Cambia el Numero de Score por un Numero porcentual de progreso del nivel.

---
+++ Assets/Scripts/Progress.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform END;
+    public Text scoreText;
+    public float playerPosition = 0f;
+
+
+    void Update()
+    {
+        if (!FindObjectOfType<GameManager>().GetGameHasEndend() && playerPosition < 100f)
+        {
+            //Debug.Log(playerPosition);
+            //scoreText.text = player.position.z.ToString("0");
+            playerPosition = (player.position.z * 100) / END.position.z;
+            scoreText.text = playerPosition.ToString("0") + "%";
+        }
+        
+    }
+}
